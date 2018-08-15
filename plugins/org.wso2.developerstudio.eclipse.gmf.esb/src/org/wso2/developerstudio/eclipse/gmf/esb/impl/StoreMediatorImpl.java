8ff09bfca68055dee2b1c79885983f217cc0f591
Rosen Silva <rosen@live.com>
2018-08-15 16:43:19 +0530

    add tooling support for Xpath expressions in store mediator

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb/src/org/wso2/developerstudio/eclipse/gmf/esb/impl/StoreMediatorImpl.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb/src/org/wso2/developerstudio/eclipse/gmf/esb/impl/StoreMediatorImpl.java
@@ -16,17 +16,18 @@ import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.wso2.developerstudio.eclipse.esb.core.utils.ESBMediaTypeConstants;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbPackage;
+import org.wso2.developerstudio.eclipse.gmf.esb.NamespacedProperty;
 import org.wso2.developerstudio.eclipse.gmf.esb.RegistryKeyProperty;
 import org.wso2.developerstudio.eclipse.gmf.esb.StoreMediator;
 import org.wso2.developerstudio.eclipse.gmf.esb.StoreMediatorInputConnector;
 import org.wso2.developerstudio.eclipse.gmf.esb.StoreMediatorOutputConnector;
+import org.wso2.developerstudio.eclipse.gmf.esb.StoreMediatorSpecifyType;
 import org.wso2.developerstudio.eclipse.platform.core.utils.CSProviderConstants;
 import org.wso2.developerstudio.eclipse.platform.core.utils.DeveloperStudioProviderUtils;
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Store Mediator</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> An implementation of the model object '<em><b>Store
+ * Mediator</b></em>'. <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
  * </p>
@@ -36,6 +37,8 @@ import org.wso2.developerstudio.eclipse.platform.core.utils.DeveloperStudioProvi
  *   <li>{@link org.wso2.developerstudio.eclipse.gmf.esb.impl.StoreMediatorImpl#getInputConnector <em>Input Connector</em>}</li>
  *   <li>{@link org.wso2.developerstudio.eclipse.gmf.esb.impl.StoreMediatorImpl#getOutputConnector <em>Output Connector</em>}</li>
  *   <li>{@link org.wso2.developerstudio.eclipse.gmf.esb.impl.StoreMediatorImpl#getAvailableMessageStores <em>Available Message Stores</em>}</li>
+ *   <li>{@link org.wso2.developerstudio.eclipse.gmf.esb.impl.StoreMediatorImpl#getExpression <em>Expression</em>}</li>
+ *   <li>{@link org.wso2.developerstudio.eclipse.gmf.esb.impl.StoreMediatorImpl#getSpecifyAs <em>Specify As</em>}</li>
  * </ul>
  *
  * @generated
@@ -43,8 +46,7 @@ import org.wso2.developerstudio.eclipse.platform.core.utils.DeveloperStudioProvi
 public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	/**
 	 * The default value of the '{@link #getMessageStore() <em>Message Store</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @see #getMessageStore()
 	 * @generated
 	 * @ordered
@@ -53,8 +55,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 
 	/**
 	 * The cached value of the '{@link #getMessageStore() <em>Message Store</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @see #getMessageStore()
 	 * @generated
 	 * @ordered
@@ -63,8 +64,8 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 
 	/**
 	 * The cached value of the '{@link #getOnStoreSequence() <em>On Store Sequence</em>}' containment reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * @see #getOnStoreSequence()
 	 * @generated
 	 * @ordered
@@ -73,8 +74,8 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 
 	/**
 	 * The cached value of the '{@link #getInputConnector() <em>Input Connector</em>}' containment reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * @see #getInputConnector()
 	 * @generated
 	 * @ordered
@@ -83,8 +84,8 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 
 	/**
 	 * The cached value of the '{@link #getOutputConnector() <em>Output Connector</em>}' containment reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * @see #getOutputConnector()
 	 * @generated
 	 * @ordered
@@ -92,9 +93,10 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	protected StoreMediatorOutputConnector outputConnector;
 
 	/**
-	 * The default value of the '{@link #getAvailableMessageStores() <em>Available Message Stores</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The default value of the '{@link #getAvailableMessageStores() <em>Available
+	 * Message Stores</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
 	 * @see #getAvailableMessageStores()
 	 * @generated
 	 * @ordered
@@ -102,9 +104,10 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	protected static final String AVAILABLE_MESSAGE_STORES_EDEFAULT = null;
 
 	/**
-	 * The cached value of the '{@link #getAvailableMessageStores() <em>Available Message Stores</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The cached value of the '{@link #getAvailableMessageStores() <em>Available
+	 * Message Stores</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
 	 * @see #getAvailableMessageStores()
 	 * @generated
 	 * @ordered
@@ -112,8 +115,37 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	protected String availableMessageStores = AVAILABLE_MESSAGE_STORES_EDEFAULT;
 
 	/**
+	 * The cached value of the '{@link #getExpression() <em>Expression</em>}' containment reference.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @see #getExpression()
+	 * @generated
+	 * @ordered
+	 */
+	protected NamespacedProperty expression;
+
+	/**
+	 * The default value of the '{@link #getSpecifyAs() <em>Specify As</em>}' attribute.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * @see #getSpecifyAs()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final StoreMediatorSpecifyType SPECIFY_AS_EDEFAULT = StoreMediatorSpecifyType.VALUE;
+
+	/**
+	 * The cached value of the '{@link #getSpecifyAs() <em>Specify As</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getSpecifyAs()
+	 * @generated
+	 * @ordered
+	 */
+	protected StoreMediatorSpecifyType specifyAs = SPECIFY_AS_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated NOT
 	 */
 	@SuppressWarnings("unchecked")
@@ -121,30 +153,34 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 		super();
 		setMessageStore("messageStore");
 		onStoreSequence = EsbFactoryImpl.eINSTANCE.createRegistryKeyProperty();
-		DeveloperStudioProviderUtils.addFilter(
-				(Map<String, List<String>>) onStoreSequence.getFilters(),
-				CSProviderConstants.FILTER_MEDIA_TYPE,
-				ESBMediaTypeConstants.MEDIA_TYPE_SEQUENCE);
+		DeveloperStudioProviderUtils.addFilter((Map<String, List<String>>) onStoreSequence.getFilters(),
+				CSProviderConstants.FILTER_MEDIA_TYPE, ESBMediaTypeConstants.MEDIA_TYPE_SEQUENCE);
 		onStoreSequence.setPrettyName("onStore Sequence");
 		onStoreSequence.setKeyName("onStore Sequence");
 		onStoreSequence.setKeyValue("");
 		setOnStoreSequence(onStoreSequence);
+
+		// Payload xpath.
+		NamespacedProperty payloadXpath = EsbFactoryImpl.eINSTANCE.createNamespacedProperty();
+		payloadXpath.setPrettyName("Payload XPath");
+		payloadXpath.setPropertyName("xpath");
+		payloadXpath.setPropertyValue(DEFAULT_XPATH_PROPERTY_VALUE);
+		setExpression(payloadXpath);
+
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	protected EClass eStaticClass() {
 		return EsbPackage.Literals.STORE_MEDIATOR;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public String getMessageStore() {
@@ -152,8 +188,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public void setMessageStore(String newMessageStore) {
@@ -164,8 +199,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public RegistryKeyProperty getOnStoreSequence() {
@@ -173,8 +207,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public NotificationChain basicSetOnStoreSequence(RegistryKeyProperty newOnStoreSequence, NotificationChain msgs) {
@@ -188,8 +221,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public void setOnStoreSequence(RegistryKeyProperty newOnStoreSequence) {
@@ -207,8 +239,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public StoreMediatorInputConnector getInputConnector() {
@@ -216,11 +247,11 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	public NotificationChain basicSetInputConnector(StoreMediatorInputConnector newInputConnector, NotificationChain msgs) {
+	public NotificationChain basicSetInputConnector(StoreMediatorInputConnector newInputConnector,
+			NotificationChain msgs) {
 		StoreMediatorInputConnector oldInputConnector = inputConnector;
 		inputConnector = newInputConnector;
 		if (eNotificationRequired()) {
@@ -231,8 +262,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public void setInputConnector(StoreMediatorInputConnector newInputConnector) {
@@ -250,8 +280,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public StoreMediatorOutputConnector getOutputConnector() {
@@ -259,11 +288,11 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	public NotificationChain basicSetOutputConnector(StoreMediatorOutputConnector newOutputConnector, NotificationChain msgs) {
+	public NotificationChain basicSetOutputConnector(StoreMediatorOutputConnector newOutputConnector,
+			NotificationChain msgs) {
 		StoreMediatorOutputConnector oldOutputConnector = outputConnector;
 		outputConnector = newOutputConnector;
 		if (eNotificationRequired()) {
@@ -274,8 +303,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public void setOutputConnector(StoreMediatorOutputConnector newOutputConnector) {
@@ -293,8 +321,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public String getAvailableMessageStores() {
@@ -302,8 +329,7 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public void setAvailableMessageStores(String newAvailableMessageStores) {
@@ -314,8 +340,68 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 	}
 
 	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NamespacedProperty getExpression() {
+		return expression;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetExpression(NamespacedProperty newExpression, NotificationChain msgs) {
+		NamespacedProperty oldExpression = expression;
+		expression = newExpression;
+		if (eNotificationRequired()) {
+			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, EsbPackage.STORE_MEDIATOR__EXPRESSION, oldExpression, newExpression);
+			if (msgs == null) msgs = notification; else msgs.add(notification);
+		}
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setExpression(NamespacedProperty newExpression) {
+		if (newExpression != expression) {
+			NotificationChain msgs = null;
+			if (expression != null)
+				msgs = ((InternalEObject)expression).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - EsbPackage.STORE_MEDIATOR__EXPRESSION, null, msgs);
+			if (newExpression != null)
+				msgs = ((InternalEObject)newExpression).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - EsbPackage.STORE_MEDIATOR__EXPRESSION, null, msgs);
+			msgs = basicSetExpression(newExpression, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, EsbPackage.STORE_MEDIATOR__EXPRESSION, newExpression, newExpression));
+	}
+
+	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StoreMediatorSpecifyType getSpecifyAs() {
+		return specifyAs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setSpecifyAs(StoreMediatorSpecifyType newSpecifyAs) {
+		StoreMediatorSpecifyType oldSpecifyAs = specifyAs;
+		specifyAs = newSpecifyAs == null ? SPECIFY_AS_EDEFAULT : newSpecifyAs;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, EsbPackage.STORE_MEDIATOR__SPECIFY_AS, oldSpecifyAs, specifyAs));
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	@Override
@@ -327,16 +413,17 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 				return basicSetInputConnector(null, msgs);
 			case EsbPackage.STORE_MEDIATOR__OUTPUT_CONNECTOR:
 				return basicSetOutputConnector(null, msgs);
+			case EsbPackage.STORE_MEDIATOR__EXPRESSION:
+				return basicSetExpression(null, msgs);
 		}
 		return super.eInverseRemove(otherEnd, featureID, msgs);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
@@ -350,16 +437,19 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 				return getOutputConnector();
 			case EsbPackage.STORE_MEDIATOR__AVAILABLE_MESSAGE_STORES:
 				return getAvailableMessageStores();
+			case EsbPackage.STORE_MEDIATOR__EXPRESSION:
+				return getExpression();
+			case EsbPackage.STORE_MEDIATOR__SPECIFY_AS:
+				return getSpecifyAs();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
@@ -378,16 +468,21 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 			case EsbPackage.STORE_MEDIATOR__AVAILABLE_MESSAGE_STORES:
 				setAvailableMessageStores((String)newValue);
 				return;
+			case EsbPackage.STORE_MEDIATOR__EXPRESSION:
+				setExpression((NamespacedProperty)newValue);
+				return;
+			case EsbPackage.STORE_MEDIATOR__SPECIFY_AS:
+				setSpecifyAs((StoreMediatorSpecifyType)newValue);
+				return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
@@ -406,16 +501,21 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 			case EsbPackage.STORE_MEDIATOR__AVAILABLE_MESSAGE_STORES:
 				setAvailableMessageStores(AVAILABLE_MESSAGE_STORES_EDEFAULT);
 				return;
+			case EsbPackage.STORE_MEDIATOR__EXPRESSION:
+				setExpression((NamespacedProperty)null);
+				return;
+			case EsbPackage.STORE_MEDIATOR__SPECIFY_AS:
+				setSpecifyAs(SPECIFY_AS_EDEFAULT);
+				return;
 		}
 		super.eUnset(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
@@ -429,16 +529,19 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 				return outputConnector != null;
 			case EsbPackage.STORE_MEDIATOR__AVAILABLE_MESSAGE_STORES:
 				return AVAILABLE_MESSAGE_STORES_EDEFAULT == null ? availableMessageStores != null : !AVAILABLE_MESSAGE_STORES_EDEFAULT.equals(availableMessageStores);
+			case EsbPackage.STORE_MEDIATOR__EXPRESSION:
+				return expression != null;
+			case EsbPackage.STORE_MEDIATOR__SPECIFY_AS:
+				return specifyAs != SPECIFY_AS_EDEFAULT;
 		}
 		return super.eIsSet(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
-	
+
 	@Override
 	public String toString() {
 		if (eIsProxy()) return super.toString();
@@ -448,8 +551,10 @@ public class StoreMediatorImpl extends MediatorImpl implements StoreMediator {
 		result.append(messageStore);
 		result.append(", availableMessageStores: ");
 		result.append(availableMessageStores);
+		result.append(", SpecifyAs: ");
+		result.append(specifyAs);
 		result.append(')');
 		return result.toString();
 	}
 
-} //StoreMediatorImpl
+} // StoreMediatorImpl
