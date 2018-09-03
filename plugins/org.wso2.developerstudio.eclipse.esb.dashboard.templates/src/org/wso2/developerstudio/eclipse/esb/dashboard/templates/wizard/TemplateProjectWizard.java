6f2349229b0b1fb8fcc13120883a05f68fb9c66c
Nirothipan <nirothipan@gmail.com>
2018-09-03 10:08:14 +0530

    Add capp creation for sample project

--- plugins/org.wso2.developerstudio.eclipse.esb.dashboard.templates/src/org/wso2/developerstudio/eclipse/esb/dashboard/templates/wizard/TemplateProjectWizard.java
+++ plugins/org.wso2.developerstudio.eclipse.esb.dashboard.templates/src/org/wso2/developerstudio/eclipse/esb/dashboard/templates/wizard/TemplateProjectWizard.java
@@ -5,8 +5,14 @@ package org.wso2.developerstudio.eclipse.esb.dashboard.templates.wizard;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.regex.Pattern;
 
+import org.apache.maven.model.Dependency;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
@@ -15,6 +21,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -31,7 +38,9 @@ import org.osgi.framework.Bundle;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBArtifact;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBProjectArtifact;
 import org.wso2.developerstudio.eclipse.maven.util.MavenUtils;
+import org.wso2.developerstudio.eclipse.platform.core.utils.DeveloperStudioProviderUtils;
 import org.wso2.developerstudio.eclipse.utils.file.FileUtils;
+import org.wso2.developerstudio.eclipse.utils.project.ProjectUtils;
 import org.wso2.developerstudio.eclipse.utils.template.TemplateUtil;
 
 public class TemplateProjectWizard extends Wizard implements INewWizard {
@@ -39,11 +48,15 @@ public class TemplateProjectWizard extends Wizard implements INewWizard {
     private TemplateProjectWizardPage page;
     private ISelection selection;
     private TemplateWizardUtil templateWizardUtil;
-    private File pomfile;
+    // private File pomfile;
     private String groupId;
     private String version = "1.0.0";
-    ESBProjectArtifact esbProjectArtifact;
-    String name = "HelloWorld";
+    // ESBProjectArtifact esbProjectArtifact;
+
+    private static final String DISTRIBUTION_PROJECT_NATURE = "org.wso2.developerstudio.eclipse.distribution.project.nature";
+
+    String sampleName = "HelloWorld";
+    String containerName;
 
     public TemplateProjectWizard() {
         super();
@@ -68,7 +81,7 @@ public class TemplateProjectWizard extends Wizard implements INewWizard {
     @Override
     public boolean performFinish() {
 
-        final String containerName = page.getContainerName();
+        containerName = page.getContainerName();
 
         IRunnableWithProgress op = new IRunnableWithProgress() {
             @Override
@@ -95,28 +108,41 @@ public class TemplateProjectWizard extends Wizard implements INewWizard {
         return true;
     }
 
-    private void copyFiles(IProject esbProject) {
+    /**
+     * Copy each files which belongs to the samples.
+     *
+     * @param esbProject
+     * @param esbProjectArtifact
+     */
+    private void copyFiles(IProject esbProject, ESBProjectArtifact esbProjectArtifact) {
+
+        // Copy HelloWorld Proxy
+        copyProxy(esbProject, sampleName, sampleName, esbProjectArtifact);
+
+    }
+
+    private void copyProxy(IProject esbProject, String sampleName, String proxyName,
+            ESBProjectArtifact esbProjectArtifact) {
 
         IContainer location = esbProject.getFolder(
                 "src" + File.separator + "main" + File.separator + "synapse-config" + File.separator
                         + "proxy-services");
+
         try {
             File importFile = getSampleResourceFile();
-            String sampleFileName = name + ".xml";
-            File sampleFile = new File(sampleFileName);
-            IFile proxyServiceFile = location.getFile(new Path(sampleFile.getName()));
+            IFile proxyServiceFile = location.getFile(new Path(proxyName + ".xml"));
             File destFile = proxyServiceFile.getLocation().toFile();
             FileUtils.copy(importFile, destFile);
             String grpID = groupId + ".proxy-service";
             String relativePath = FileUtils.getRelativePath(location.getProject().getLocation().toFile(),
-                    new File(location.getLocation().toFile(), sampleFileName))
+                    new File(location.getLocation().toFile(), proxyName + ".xml"))
                     .replaceAll(Pattern.quote(File.separator), "/");
-            esbProjectArtifact.addESBArtifact(createArtifact(sampleFileName, grpID, version, relativePath));
-            esbProjectArtifact.toFile();
-            esbProject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-        } catch (Exception e) {
+            esbProjectArtifact.addESBArtifact(createArtifact(proxyName, grpID, version, relativePath));
+        } catch (IOException e) {
             // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+
     }
 
     protected File getSampleResourceFile() throws IOException {
@@ -141,30 +167,121 @@ public class TemplateProjectWizard extends Wizard implements INewWizard {
      */
     private void doFinish(String containerName, IProgressMonitor monitor) throws CoreException {
         IProject project = createProject(containerName, TemplateProjectConstants.ESB_PROJECT_NATURE);
-        pomfile = project.getFile("pom.xml").getLocation().toFile();
+        File pomfile = project.getFile("pom.xml").getLocation().toFile();
         try {
-            createPOM(pomfile, containerName);
+
+            groupId = "wso2.sampleTemplate." + containerName;
+
+            createPOM(groupId, pomfile, containerName, "pom");
             templateWizardUtil.addNature(project, TemplateProjectConstants.ESB_PROJECT_NATURE);
-            esbProjectArtifact = new ESBProjectArtifact();
-            IFile file = project.getFile("artifact.xml");
-            esbProjectArtifact.setSource(file.getLocation().toFile());
+            ESBProjectArtifact esbProjectArtifact = new ESBProjectArtifact();
+            IFile artifactXML = project.getFile("artifact.xml");
+            esbProjectArtifact.setSource(artifactXML.getLocation().toFile());
             esbProjectArtifact.toFile();
-            copyFiles(project);
+
+            copyFiles(project, esbProjectArtifact);
+
+            esbProjectArtifact.toFile();
+
+            project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+
+            carbonAppCreation(containerName + "CarbonApplication");
+
         } catch (Exception ex) {
             templateWizardUtil.throwCoreException("Error creating pom file for project " + containerName, ex);
         }
     }
 
+    private void carbonAppCreation(String projectName) {
+
+        try {
+
+            IProject CarbonAppProject = createNewProject(projectName);
+
+            File pomfile = CarbonAppProject.getFile("pom.xml").getLocation().toFile();
+            createPOM(pomfile);
+            ProjectUtils.addNatureToProject(CarbonAppProject, false, DISTRIBUTION_PROJECT_NATURE);
+            MavenUtils.updateWithMavenEclipsePlugin(pomfile, new String[] {},
+                    new String[] { DISTRIBUTION_PROJECT_NATURE });
+            CarbonAppProject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+
+            List<Dependency> dependencyList = new ArrayList<Dependency>();
+            MavenProject mavenProject = MavenUtils.getMavenProject(pomfile);
+            Properties properties = mavenProject.getModel().getProperties();
+
+            Dependency dependency = new Dependency();
+
+            dependency.setGroupId(groupId + ".proxy-service");
+            dependency.setArtifactId(sampleName + ".xml");
+            dependency.setVersion("1.0.0");
+            dependency.setType("xml");
+
+            dependencyList.add(dependency);
+            properties.put(getArtifactInfoAsString(dependency), "capp/EnterpriseServiceBus");
+
+            ArtifactTypeMapping artifactTypeMapping = new ArtifactTypeMapping();
+            properties.put("artifact.types", artifactTypeMapping.getArtifactTypes());
+
+            mavenProject.getModel().setProperties(properties);
+
+            MavenUtils.addMavenDependency(mavenProject, dependencyList);
+            MavenUtils.saveMavenProject(mavenProject, pomfile);
+            CarbonAppProject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getArtifactInfoAsString(Dependency dep) {
+        String suffix = "";
+
+        String str = suffix.concat(dep.getGroupId().concat("_._").concat(dep.getArtifactId()));
+        System.out.println("str get artifact = " + str);
+        return str;
+    }
+
+    public void createPOM(File pomLocation) throws Exception {
+
+        MavenProject mavenProject = MavenUtils
+                .createMavenProject(groupId, containerName + "CarbonApp", version, "carbon/application");
+
+        MavenUtils.saveMavenProject(mavenProject, pomLocation);
+    }
+
+    public IProject createNewProject(String name) throws CoreException {
+
+        IProject project = null;
+
+        project = createProjectInDefaultWorkspace(name);
+
+        return project;
+    }
+
+    private IProject createProjectInDefaultWorkspace(String name) throws CoreException {
+
+        IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+        IProject project = root.getProject(name);
+        project.create(new NullProgressMonitor());
+        project.open(new NullProgressMonitor());
+
+        return project;
+    }
+
     /**
-     * Create the pom file for the template project.
+     * Create the POM file for the project.
      *
      * @param pomLocation
      * @param projectName
      * @throws Exception
      */
-    public void createPOM(File pomLocation, String projectName) throws Exception {
-        groupId = "wso2.template." + projectName;
-        MavenProject mavenProject = MavenUtils.createMavenProject(groupId, projectName, version, "pom");
+    public void createPOM(String groupID, File pomLocation, String projectName, String packaging) throws Exception {
+        MavenProject mavenProject = MavenUtils.createMavenProject(groupID, projectName, version, packaging);
+        MavenUtils.updateMavenRepo(mavenProject);
+//        mavenProject.getModel().addRepository(globalRepositoryFromPreference);
+//	mavenProject.getModel().addPluginRepository(globalRepositoryFromPreference);
         MavenUtils.saveMavenProject(mavenProject, pomLocation);
     }
 
@@ -198,6 +315,7 @@ public class TemplateProjectWizard extends Wizard implements INewWizard {
         this.selection = selection;
         setHelpAvailable(true);
     }
+
 }
 
 class ProxyServiceTemplateUtils extends TemplateUtil {
@@ -215,3 +333,54 @@ class ProxyServiceTemplateUtils extends TemplateUtil {
         return Platform.getBundle(org.wso2.developerstudio.eclipse.esb.dashboard.templates.Activator.PLUGIN_ID);
     }
 }
+
+class ArtifactTypeMapping {
+    private static final String ARTIFACT_TYPE = "artifactType";
+    private static final String FILE_EXTENSION = "fileExtension";
+    private static final String REGISTER_ARTIFACT_MAPPING_EXTENSION_ID = "org.wso2.developerstudio.register.artifact.mapping";
+    private static Map<String, String> type = new HashMap<String, String>();
+    private static Map<String, String> subType = new HashMap<String, String>();
+
+    public ArtifactTypeMapping() {
+        DeveloperStudioProviderUtils devStudioUtils = new DeveloperStudioProviderUtils();
+        IConfigurationElement[] artifactMappings = devStudioUtils
+                .getExtensionPointmembers(REGISTER_ARTIFACT_MAPPING_EXTENSION_ID);
+        for (IConfigurationElement artifactmapping : artifactMappings) {
+            type.put(artifactmapping.getAttribute(ARTIFACT_TYPE), artifactmapping.getAttribute(FILE_EXTENSION));
+        }
+        subType.put("jar", "jar");
+        subType.put("bundle", "jar");
+
+    }
+
+    public boolean isValidArtifactType(final String str) {
+        return type.containsKey(str);
+    }
+
+    public String getType(final String packaging) {
+        String value = "";
+        if (type.containsKey(packaging)) {
+            value = type.get(packaging);
+        } else {
+            if (subType.containsKey(packaging)) {
+                value = subType.get(packaging);
+            } else {
+                value = packaging.replaceAll("/", "_");
+            }
+        }
+        return value;
+    }
+
+    public String getArtifactTypes() {
+        StringBuffer artifactTypes = new StringBuffer();
+        for (String key : type.keySet()) {
+            artifactTypes.append(key);
+            artifactTypes.append("=");
+            artifactTypes.append(type.get(key));
+            artifactTypes.append(',');
+        }
+        return artifactTypes.toString().replaceAll(",$", "");
+    }
+
+}
+
